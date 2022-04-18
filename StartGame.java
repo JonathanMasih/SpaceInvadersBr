484838df0ad552742bc3f16c3336e9a57a05abfe
JonathanMasih <j24masi@siena.edu>
2022-04-18 10:16:46 -0400

    background image added to the GUI

--- StartGame.java
+++ StartGame.java
@@ -3,7 +3,6 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-
 /**
  * This GUI will display two buttons with options for playing the single player
  * mode
@@ -15,21 +14,18 @@ import javax.swing.*;
 public class StartGame implements ActionListener, Runnable {
     private JButton onePlayer;
     private JButton twoPlayer;
-   
+    private static Image backgroundImage;
+    private JFrame frame;
+    private JPanel backGroundPanel;
+
     /**
      * The run method to set up the graphical user interface
      */
-
     @Override
     public void run() {
-
-        // set up the GUI "look and feel" which should match
-        // the OS on which we are running
-        JFrame.setDefaultLookAndFeelDecorated(true);
-
         // create a JFrame in which we will build our very
         // tiny GUI, and give the window a name
-        JFrame frame = new JFrame("Welcome to Space Invaders!");
+        frame = new JFrame("Welcome to Space Invaders!");
         frame.setPreferredSize(new Dimension(800, 800));
 
         // tell the JFrame that when someone closes the
@@ -37,15 +33,28 @@ public class StartGame implements ActionListener, Runnable {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // JPanel for the buttons to pick the game mode
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout( panel, BoxLayout.PAGE_AXIS));
+        JPanel gameModePanel = new JPanel();
+        gameModePanel.setLayout(new BoxLayout(gameModePanel, BoxLayout.PAGE_AXIS));
         JPanel centerPanel = new JPanel(new BorderLayout());
-        
-        //to center buttons and selection label, make panel layout manager null
-        //and position them in center of the panel with font metrics
+
+        // Setting the background of the frame
+        backGroundPanel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                // first, we should call the paintComponent method we are
+                // overriding in JPanel
+                super.paintComponent(g);
+                // draw the snowflake
+                g.drawImage(backgroundImage, 0, 0, frame.getWidth(), frame.getHeight(), this);
+            }
+        };
+
+        // to center buttons and selection label, make panel layout manager null
+        // and position them in center of the panel with font metrics
         JLabel selection = new JLabel("Please select a game mode.");
         selection.setPreferredSize(new Dimension(300, 50));
         selection.setFont(new Font("Verdana", Font.PLAIN, 20));
+        selection.setForeground(Color.BLUE);
         onePlayer = new JButton("Single Player");
         onePlayer.setFont(new Font("Verdana", Font.PLAIN, 20));
         onePlayer.setPreferredSize(new Dimension(200, 50));
@@ -54,47 +63,62 @@ public class StartGame implements ActionListener, Runnable {
         twoPlayer.setPreferredSize(new Dimension(200, 50));
         onePlayer.addActionListener(this);
         twoPlayer.addActionListener(this);
-        
+
         JPanel selectionLabelPanel = new JPanel();
         selectionLabelPanel.add(selection);
         selectionLabelPanel.setPreferredSize(new Dimension(300, 50));
+        selectionLabelPanel.setOpaque(false);
         JPanel oneplayerButtonPanel = new JPanel();
         oneplayerButtonPanel.setPreferredSize(new Dimension(200, 50));
         oneplayerButtonPanel.add(onePlayer);
+        oneplayerButtonPanel.setOpaque(false);
         JPanel twoPlayerButtonPanel = new JPanel();
         twoPlayerButtonPanel.setPreferredSize(new Dimension(200, 50));
         twoPlayerButtonPanel.add(twoPlayer);
+        twoPlayerButtonPanel.setOpaque(false);
+        gameModePanel.add(selectionLabelPanel);
+        gameModePanel.add(oneplayerButtonPanel);
+        gameModePanel.add(twoPlayerButtonPanel);
 
+        // makes the gameModePanel transparent
+        gameModePanel.setOpaque(false);
+        centerPanel.setOpaque(false);
+        centerPanel.add(gameModePanel, BorderLayout.CENTER);
 
-        panel.add(selectionLabelPanel);
-        panel.add(oneplayerButtonPanel);
-        panel.add(twoPlayerButtonPanel);
-        centerPanel.add(panel , BorderLayout.CENTER);
-        
+        onePlayer.addActionListener(this);
+        twoPlayer.addActionListener(this);
+        // Sets the background of the frame to space image
+        frame.setContentPane(backGroundPanel);
         frame.add(centerPanel);
-
         // display the window we've created
         frame.pack();
         frame.setVisible(true);
     }
 
+    /**
+     * Checks which action is performed and does something
+     * depending on the action.
+     * 
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == onePlayer) {
-           // javax.swing.SwingUtilities.invokeLater(new SinglePlayer());
+            // javax.swing.SwingUtilities.invokeLater(new SinglePlayer());
         }
         if (e.getSource() == twoPlayer) {
-           // javax.swing.SwingUtilities.invokeLater(new DoublePlayer());
+            // javax.swing.SwingUtilities.invokeLater(new DoublePlayer());
         }
 
     }
 
     public static void main(String[] args) {
-        //"javax.swing.plaf.nimbus.NimbusLookAndFeel")
+        // "javax.swing.plaf.nimbus.NimbusLookAndFeel")
         try {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-        }catch(Exception e){
+        } catch (Exception e) {
         }
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        backgroundImage = toolkit.getImage("background.gif");
         javax.swing.SwingUtilities.invokeLater(new StartGame());
     }
 
