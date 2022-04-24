ed343d19c662910262b9639c26874e1d9d4b05ab
TCollins443 <t09coll@siena.edu>
2022-04-24 19:36:25 -0400

    Progress on enemy player

--- EnemyPlayer.java
+++ EnemyPlayer.java
@@ -1,18 +1,23 @@
 import java.awt.Point;
 import java.awt.*;
+import java.util.*;
 
-public class EnemyPlayer extends Player{
+public class EnemyPlayer extends Player {
 
+    protected int hits;
+    protected final int DELAY_TIME = 30;
+    protected final int ENEMYYPOS = 200;
 
     public EnemyPlayer(Point startPos) {
         super(startPos);
-        //TODO Auto-generated constructor stub
+        this.hits = 5;
+        // TODO Auto-generated constructor stub
     }
 
-       /**
-     * Set the Image to be used by the player Object 
+    /**
+     * Set the Image to be used by the player Object
      */
-    //@Override
+    // @Override
     public static void loadPlayerPic() {
 
         Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -20,8 +25,21 @@ public class EnemyPlayer extends Player{
     }
 
     @Override
-    public void run(){
-        
+    public void run() {
+        Random rand = new Random();
+        int movementSpeedx = 0;
+         movementSpeedx += rand.nextInt(11) - 10;
+        while (hits != 0) {
+            try {
+                sleep(DELAY_TIME);
+            } catch (InterruptedException e) {
+            }
+            if(super.upperLeftOfPLayer.x +  super.PLAYERSIZE < SinglePlayer.GAME_PANEL_WIDTH){
+                movementSpeedx += rand.nextInt(11) - 10;
+            super.translate(movementSpeedx);
+            }
+            else if(super.upperLeftOfPLayer.x +  super.PLAYERSIZE > SinglePlayer.GAME_PANEL_WIDTH)
+        }
     }
-    
+
 }
