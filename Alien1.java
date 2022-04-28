8681b1d4f8c09df031f8a29156b89f1c60f474e0
JonathanMasih <j24masi@siena.edu>
2022-04-28 16:09:51 -0400

    Pushed different alien class

---
+++ Alien1.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+/**
+ * Alein type 1 that extends Alein
+ * 
+ */
+public class Alien1 extends Alien{
+    
+    protected static Image alienImage;
+    public Alien1(Point upperLeft){
+        super(upperLeft);
+    }
+      /**
+     * paint this object onto the given Graphics area
+     * 
+     * @param g the Graphics object where the shape should be drawn
+     */
+    @Override
+    public void paint(Graphics g) {
+        if (!alienHit) {
+            g.drawImage(alienImage, upperLeftOfAlien.x, upperLeftOfAlien.y, ALIENSIZE, ALIENSIZE, null);
+        }
+    }
+
+    /**
+     * Set the Image to be used by the Alien Object
+     */
+    public static void loadAlien1Pic() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        alienImage = toolkit.getImage("Alien.png");
+    }
+}
